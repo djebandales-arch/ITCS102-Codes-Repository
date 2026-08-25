f6e1e357151477d1df8fa7ea2c0400dcf282f0a4
djebandales-arch <djebandales@gmail.com>
2026-08-25 12:50:16 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,12 @@
+a = 18
+b = 2
+sum = a + b
+diff = a - b
+
+print("The sum of ",a,"plus ",b," is ", sum)
+print("The sum of ",a,"minus ",b," is ", diff)
+print("The sum of ",a,"times ",b, "is ", a*b)
+print("The sum of ",a,"times ",b, "is ", a/b)
+print("The sum of ",a,"divide ",b, "is ", a//b)
+print("The sum of ",a,"divide ",b, "is ", a**b)
+print("The sum of ",a,"divide ",b, "is ", a%b)

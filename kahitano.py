d285faac557ab3b4e5f922c0782101097bf24b34
djebandales-arch <djebandales@gmail.com>
2026-08-25 12:53:43 +0800

    Add files via upload

---
+++ kahitano.py
@@ -0,0 +1,26 @@
+#integer variable
+numberofcats = 3
+numberofdogs = 5
+mycousins = 10
+
+#float variable
+myheight = 157.4
+targetgrades = 1.25
+jhsgwa = 94.45
+
+#string variable
+myMother = "venus"
+myCat = "mali"
+myDog = "sky"
+
+#boolean variable
+isCute = True
+canCook = True
+canDance = False
+
+print("I have", numberofcats, "cats")
+print("My dogs are", numberofdogs)
+print("I have", mycousins, "cousins")
+print("My height is", myheight)
+print("My target grades is", targetgrades)
+print

c0a8b66bb85b38777432b30c6fc205feff58d646
djebandales-arch <djebandales@gmail.com>
2026-09-01 17:10:25 +0800

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,22 @@
+
+Djenelle S. Bandales
+Djenelle S. Bandales
+Djenelle S. Bandales
+Djenelle S. Bandales
+Djenelle S. Bandales
+Djenelle S. Bandales
+Djenelle S. Bandales
+Djenelle S. Bandales
+Djenelle S. Bandales
+Djenelle S. Bandales
+
+Bachelor of Science in Information Technology
+Bachelor of Science in Information Technology
+Bachelor of Science in Information Technology
+Bachelor of Science in Information Technology
+Bachelor of Science in Information Technology
+Bachelor of Science in Information Technology
+Bachelor of Science in Information Technology
+Bachelor of Science in Information Technology
+Bachelor of Science in Information Technology
+Bachelor of Science in Information Technology

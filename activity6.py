50c17cd275c86483a89be56662e8f4f8700b0889
djebandales-arch <djebandales@gmail.com>
2026-09-01 17:54:18 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ---->   ")
+
+print("Hi,", name, "! how are you doing today >< ?")
+
+response = input("are you alright? ----->   ")
+print("Well, it's a beautiful day today")
+

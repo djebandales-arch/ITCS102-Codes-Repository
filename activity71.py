7c0f7c048614ff9b38f239ca174cad9b91aad4a1
djebandales-arch <djebandales@gmail.com>
2026-09-03 12:25:49 +0800

    Add files via upload

---
+++ activity71.py
@@ -0,0 +1,35 @@
+total1 = 0
+
+n1 = eval(input("Enter a number -----> ?"))
+total += n1
+
+n2 =  eval(input("Enter a number -----> ?"))
+total -= n2
+
+n2 =  eval(input("Enter a number -----> ?"))
+total -= n2
+
+n2 =  eval(input("Enter a number -----> ?"))
+total *= n2
+
+n2 =  eval(input("Enter a number -----> ?"))
+total *= n2
+
+n2 =  eval(input("Enter a number -----> ?"))
+total += n2
+
+n2 =  eval(input("Enter a number -----> ?"))
+total += n2
+
+n2 =  eval(input("Enter a number -----> ?"))
+total += n2
+
+n2 =  eval(input("Enter a number -----> ?"))
+total += n2
+
+n2 =  eval(input("Enter a number -----> ?"))
+total /= n2
+
+
+
+

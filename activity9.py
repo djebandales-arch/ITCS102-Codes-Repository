1bb26eff3240ea392385ab1705294c6afb1e8322
djebandales-arch <djebandales@gmail.com>
2026-09-08 11:51:44 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,27 @@
+#Relational Operators Activtiy
+
+x = 9
+y = 1
+
+text1 = 'abc'
+text2 = 'abcd'
+
+#1
+print( x > y )
+
+#2
+print( x < y )
+
+#3
+print( x >= y)
+
+#4
+print( x <= y)
+
+#5
+print( text1 == text2 )
+
+#6
+print( text2 != text2 )
+
+
